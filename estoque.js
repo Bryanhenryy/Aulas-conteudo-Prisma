66a57f85520d1e0929165ca448339a978e0a1b00
bryan henry <gustavoworksminds@gmail.com>
2026-06-02 16:47:42 -0300

    Delete estoque.js

--- estoque.js
+++
@@ -1,9 +0,0 @@
-let estoqueAtual = 100
-let vendasDia = 35
-let produtosPorCaixa = 12
-estoqueAtual -= vendasDia
-estoqueAtual += 10
-estoqueExtra = estoqueAtual % produtosPorCaixa
-console.log(estoqueAtual)
-console.log(estoqueExtra)
-
