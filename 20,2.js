b04e56bc19c622430d319f302285b379df22b731
bryan henry <gustavoworksminds@gmail.com>
2026-06-09 16:52:33 -0300

    Add files via upload

---
+++ 20,2.js
@@ -0,0 +1,7 @@
+let palavra=["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"];
+
+let invertida = ""
+for(let i = palavra.length -1; i>=0; i--){
+    invertida += palavra[i]
+}
+console.log(invertida)
--- 21.js
+++ 21.js
@@ -2,12 +2,16 @@ function contarVogais(frase){
     let vogais= "aeiouAEIOU"
     let contador= 0
 
-    for(let char of frase) {
-       if(vogais.includes(char)){
-        contador ++
-       }
+for(let i=0; i < frase.length; i++){
+    let letra = frase[i];
+    if (vogais.includes(letra)){
+        contador++
     }
-    return contador
 }
-let frase= "Bom dia"
-console.log(contarVogais(frase))
+
+return contador}
+
+let minhaFrase = "O rato roeu a roupa do rei de Roma"
+let resultado = contarVogais(minhaFrase);
+
+console.log(" O número total de vogais é " +  resultado)
