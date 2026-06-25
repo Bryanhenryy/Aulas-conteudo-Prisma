2dc77929c5edac3bddfcae2bcd110bfaef5a1582
bryan henry <gustavoworksminds@gmail.com>
2026-06-25 16:56:41 -0300

    EXERCICIOS DOM

---
+++ exercise.js
@@ -0,0 +1,4 @@
+let titulo = document.getElementById("titulo-principal")
+titulo.innerText = "Olá mundo"
+let texto = document.getElementById("mensagem")
+texto.innerText = "A DOM foi manipulada com sucesso!"
---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+let titulo = document.getElementById("titulo")
+titulo.setAttribute("class", "foco")
+titulo.style.fontSize = "60px"
+titulo.style.color= "red"
+
+
+console.log(titulo)
+
+let aplicativo = document.querySelectorAll(".aplicativo")
+console.log(aplicativo)
+
+
+
