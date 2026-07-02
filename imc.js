2041741cd1dbe19f49f04dfaf99b4f7b64be0b8c
bryan henry <gustavoworksminds@gmail.com>
2026-07-02 17:01:04 -0300

    IMC

--- imc.js
+++ imc.js
@@ -1,10 +1,19 @@
-let peso = 62
-let altura= 1.70
-let imc = peso / (altura * altura)
-if(imc < 18.5){
-    console.log("Abaixo do peso")
-} else if(imc >= 18.5 && imc < 25){
-    console.log("Peso normal")
-} else {
-    console.log("Acima do peso")
-}
+function calcularIMC(){
+    let peso= parseFloat(document.getElementById("peso").value);
+    let altura= parseFloat(document.getElementById("altura").value);
+    let resultadoArea = document.getElementById("resultado")
+
+    if   (isNaN(peso) || isNaN(altura) || altura <= 0) {
+    resultadoArea.value = "Por favor, preencha os campos corretamente com valores numéricos.";
+    return;
+    }
+    let imc= peso / (altura * altura);
+    let classificacao = "";
+    
+    if (imc < 18.5){
+        classificacao = "Abaixo do peso"
+    }else if (imc >= 18.5 && imc < 30
+        classificacao = "Peso normal"
+    )
+    }
+    }
